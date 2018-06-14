eeed0577a91e4d8b836d5de53db19c4ef5c86d32
19740144@qq.com <19740144@qq.com>
2018-06-14 15:19:15 +0800

    全部提交

--- app/src/main/java/com/example/bestweather/MainActivity.java
+++ app/src/main/java/com/example/bestweather/MainActivity.java
@@ -1,7 +1,14 @@
 package com.example.bestweather;
 
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.preference.PreferenceManager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+
+import com.example.bestweather.gson.Weather;
+
+import okhttp3.internal.Internal;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +16,13 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        SharedPreferences prefs = PreferenceManager.getDefaultSharedPreferences(this);
+
+        if(prefs.getString("weather", null) != null) {
+            Intent intent = new Intent(this, WeatherActivity.class);
+            startActivity(intent);
+            finish();
+        }
     }
 }
